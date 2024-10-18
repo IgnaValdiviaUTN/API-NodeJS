b4808b986b68c3c4909a949ab7ec908a15e437ed
Igna Valdivia <ignacio.valdivia@alumnos.frm.utn.edu.ar>
2024-10-18 12:58:14 -0300

    feat: adaptar proyecto a sharp

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const faceapi = require('face-api.js');
-const canvas = require('canvas');
+const tf = require('@tensorflow/tfjs');
+const sharp = require('sharp');
 const cors = require('cors');
-
-const { Canvas, Image, ImageData, loadImage } = canvas;
-
-// Registrar canvas como el entorno de faceapi
-faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
 const app = express();
 app.use(cors());
+
 
 // Multer para manejar la subida de archivos
 const upload = multer({ dest: 'uploads/' });
@@ -37,17 +34,20 @@ app.post('/analyze', upload.single('image'), async (req, res) => {
     const imagePath = req.file.path;
 
     try {
-        // Cargar la imagen en un objeto canvas compatible
-        const img = await loadImage(imagePath);
-        const imgCanvas = new Canvas(img.width, img.height);
-        const ctx = imgCanvas.getContext('2d');
-        ctx.drawImage(img, 0, 0, img.width, img.height);
+        // Usar sharp para cargar y convertir la imagen a un formato que se pueda manipular
+        const { data, info } = await sharp(imagePath).raw().toBuffer({ resolveWithObject: true });
+
+        // Convertir la imagen a un tensor de 3 dimensiones [ancho, alto, canales (RGB)]
+        const imgTensor = tf.tensor3d(data, [info.height, info.width, 3]);
 
         // Procesar la imagen con face-api.js
         const detections = await faceapi
-            .detectSingleFace(imgCanvas)
+            .detectSingleFace(imgTensor)
             .withFaceExpressions()
             .withAgeAndGender();
+
+        // Liberar el tensor
+        imgTensor.dispose();
 
         // Filtrar solo las expressions y age
         const analysis = {
@@ -58,6 +58,7 @@ app.post('/analyze', upload.single('image'), async (req, res) => {
         // Enviar los resultados al cliente
         res.json(analysis);
     } catch (error) {
+        console.error('Error procesando la imagen:', error);
         res.status(500).send('Error procesando la imagen');
     } finally {
         // Eliminar el archivo temporal después de procesar
@@ -65,24 +66,23 @@ app.post('/analyze', upload.single('image'), async (req, res) => {
     }
 });
 
-
 // Ruta para procesar edad
 app.post('/analyze/age', upload.single('image'), async (req, res) => {
     const imagePath = req.file.path;
 
     try {
-        // Cargar la imagen en un objeto canvas compatible
-        const img = await loadImage(imagePath);
-        const imgCanvas = new Canvas(img.width, img.height);
-        const ctx = imgCanvas.getContext('2d');
-        ctx.drawImage(img, 0, 0, img.width, img.height);
+
+        const { data, info } = await sharp(imagePath).raw().toBuffer({ resolveWithObject: true });
+        const imgTensor = tf.tensor3d(data, [info.height, info.width, 3]);
 
         // Procesar la imagen con face-api.js
         const detections = await faceapi
-            .detectSingleFace(imgCanvas)
+            .detectSingleFace(imgTensor)
             .withAgeAndGender();
 
-        // Filtrar solo la age
+        imgTensor.dispose();
+
+        // Filtrar solo la edad
         const analysis = {
             age: detections.age
         };
@@ -97,8 +97,12 @@ app.post('/analyze/age', upload.single('image'), async (req, res) => {
     }
 });
 
+
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
